302453a14cdc39b1d0a060f5594451f6c0ec56d0
sushant09112004 <jadhavsushant379@gmail.com>
2025-10-06 15:32:55 +0530

    created backend

---
+++ middlewares/errorHandlers.js
@@ -0,0 +1,9 @@
+export const errorHandler = (err, req, res, next) => {
+  console.error('Error:', err.message);
+
+  const statusCode = err.status || 500;
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || 'Internal Server Error',
+  });
+};
