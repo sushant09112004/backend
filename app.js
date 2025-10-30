9979ba66ddedd62deaaa60eb7a34d77e944fe2f3
sushant09112004 <jadhavsushant379@gmail.com>
2025-10-30 23:50:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import routes from './routes/index.js';
-import {errorHandler} from "./middlewares/errorHandlers.js";
-import connectDB from './config/db.js';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import routes from "./routes/index.js";
+import { errorHandler } from "./middlewares/errorHandlers.js";
+import connectDB from "./config/db.js";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Connect to MongoDB
 connectDB();
-const allowedOrigins = ['http://localhost:4000', 'https://yourdomain.com'];
+const allowedOrigins = ["http://localhost:4000", "https://yourdomain.com"];
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
+      callback(null, true);
     } else {
-        callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
-    },
-    optionsSuccessStatus: 200,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
+  },
+  optionsSuccessStatus: 200,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
 app.use(helmet());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api', routes);
+app.use("/api", routes);
 app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-}
-
-);
+});
 export default app;
-
